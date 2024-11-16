bfb6d1ccc55b0dc86d6108a5c5d5ca18b1002b4f
Oscarbigheadfish <38318291+oscarpangty@users.noreply.github.com>
2024-11-16 17:39:04 +0000

    Update groq_ai.py

--- groq_ai.py
+++ groq_ai.py
@@ -1,2 +1,18 @@
+import requests
+from groq import Groq
+
+def get_weather(query: str) -> list:
+    """Search weatherapi to get the current weather"""
+    endpoint = f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={query}"
+    response = requests.get(endpoint)
+    data = response.json()
+
+    if data.get("location"):
+        return data
+    else:
+        return "Weather Data Not Found"
+
+
 def get_llm_decision(start_time,end_time,ac_preference):
-  return "Yes. Fine"
+  weather=get_weather("London weather now")
+  return "Yes. Fine"+weather
