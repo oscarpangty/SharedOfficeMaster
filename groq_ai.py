eb4ee65900a984b42e9b74b273d628ceddf221e7
Oscarbigheadfish <38318291+oscarpangty@users.noreply.github.com>
2024-11-17 10:42:23 +0000

    Update groq_ai.py

--- groq_ai.py
+++ groq_ai.py
@@ -27,7 +27,7 @@ def get_weather(query: str) -> list:
         return "Weather Data Not Found"
     
 
-def get_llm_decision(start_time,end_time,ac_preference):
+def get_llm_decision(start_time,end_time,ac_preference,room):
     weather=get_weather("London weather now")
     client = Groq(
         api_key=GROQ_API_KEY,
